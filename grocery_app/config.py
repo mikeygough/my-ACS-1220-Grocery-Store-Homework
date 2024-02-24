06a5048606e8ba4d321e2c460e7da39f03e0df2f
mikey <mikey@hey.com>
2024-02-23 18:10:55 -0600

    latest changes

--- grocery_app/config.py
+++ grocery_app/config.py
@@ -4,9 +4,10 @@ import os
 
 load_dotenv()
 
+
 class Config(object):
     """Set environment variables."""
 
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
